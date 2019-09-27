60ea50715b34266b60814ee22ddae5e0fb3bcc9c
rurbano3d <rurbano3d@gmail.com>
2019-09-26 22:44:42 -0300

    feat:reactotron, immer

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
   MdDelete,
 } from 'react-icons/md';
-
+import * as CartActions from '../../store/modules/cart/actions';
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart, removeFromCart }) {
   return (
     <Container>
       <ProductTable>
@@ -20,37 +22,41 @@ export default function Cart() {
             <th />
           </tr>
         </thead>
-        <tr>
-          <td>
-            <img
-              src="https://www.girafa.com.br/visao/default/img/produtos/cordas/guitarras/85549-1470666656-guitarra-stratocaster-yamaha-pacifica-012-black-com-22-trastes-3.jpg"
-              alt=""
-            />
-          </td>
-          <td>
-            <strong>Tênis</strong>
-            <span>R$129,90</span>
-          </td>
-          <td>
-            <div>
-              <button type="button">
-                <MdRemoveCircleOutline size={20} color="#7159c1" />
-              </button>
-              <input type="Number" readOnly value={1} />
-              <button type="button">
-                <MdAddCircleOutline size={20} color="#7159c1" />
-              </button>
-            </div>
-          </td>
-          <td>
-            <strong>R$258,80</strong>
-          </td>
-          <td>
-            <button type="button">
-              <MdDelete size={20} color="#7159c1" />
-            </button>
-          </td>
-        </tr>
+        <tbody>
+          {cart.map(product => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="Number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,80</strong>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() => removeFromCart(product.id)}
+                >
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </ProductTable>
       <footer>
         <button type="button">Finalizar Pedido</button>
@@ -62,3 +68,14 @@ export default function Cart() {
     </Container>
   );
 }
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Cart);
